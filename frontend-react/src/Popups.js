e4fbc133fd631c463b1d71839b1d0aa5c8b49237
AlexSchmitz222 <alexschmitz222@gmail.com>
2021-12-06 23:16:01 +0100

    fixed not working shit

--- frontend-react/src/Popups.js
+++ frontend-react/src/Popups.js
@@ -4,83 +4,80 @@ import { Button, Modal, Form } from "react-bootstrap";
 import { BsExclamationTriangle } from "react-icons/bs";
 
 
-function popup_new_event {
-    render() {
+function popup_new_event() {
 
-        const [show_new_event, setShow_new_event] = useState(false);
-        const handleClose_new_event = () => setShow_new_event(false);
-        const handleShow_new_event = () => setShow_new_event(true);
+    const [show_new_event, setShow_new_event] = useState(false);
+    const handleClose_new_event = () => setShow_new_event(false);
+    const handleShow_new_event = () => setShow_new_event(true);
 
-        return (
-            <Modal
-                show={show_new_event}
-                onHide={handleClose_new_event}
-                backdrop="static"
-                keyboard={false}
-            >
-                <Modal.Header>
-                    <Modal.Title>Neues Event</Modal.Title>
-                </Modal.Header>
+    return (
+        <Modal
+            show={show_new_event}
+            onHide={handleClose_new_event}
+            backdrop="static"
+            keyboard={false}
+        >
+            <Modal.Header>
+                <Modal.Title>Neues Event</Modal.Title>
+            </Modal.Header>
 
-                <Modal.Body>
-                    <Form>
-                        <Form.Group className="mb-3" controlId="inputEventTitle">
-                            <Form.Label>Titel</Form.Label>
-                            <Form.Control type="text" />
-                            <Form.Text className="text-muted">
-                            </Form.Text>
-                        </Form.Group>
+            <Modal.Body>
+                <Form>
+                    <Form.Group className="mb-3" controlId="inputEventTitle">
+                        <Form.Label>Titel</Form.Label>
+                        <Form.Control type="text" />
+                        <Form.Text className="text-muted">
+                        </Form.Text>
+                    </Form.Group>
 
-                        <Form.Group className="mb-3" controlId="inputEventDescription">
-                            <Form.Label>Beschreibung</Form.Label>
-                            <Form.Control as="textarea"  style={{ height: '100px' }}/>
-                        </Form.Group>
-                    </Form>
-                </Modal.Body>
-                <Modal.Footer>
-                    <Button variant="secondary" onClick={handleClose_new_event}>
-                        Abbrechen
-                    </Button>
-                    <Button variant="primary" onClick={handleClose_new_event}>
-                        Speichern
-                    </Button>
-                </Modal.Footer>
-            </Modal>
-        )
-    }
+                    <Form.Group className="mb-3" controlId="inputEventDescription">
+                        <Form.Label>Beschreibung</Form.Label>
+                        <Form.Control as="textarea"  style={{ height: '100px' }}/>
+                    </Form.Group>
+                </Form>
+            </Modal.Body>
+            <Modal.Footer>
+                <Button variant="secondary" onClick={handleClose_new_event}>
+                    Abbrechen
+                </Button>
+                <Button variant="primary" onClick={handleClose_new_event}>
+                    Speichern
+                </Button>
+            </Modal.Footer>
+        </Modal>
+    )
 }
 
 
-function popup_delete_event extends React.Component {
-    render() {
-        const [show_delete_event, setShow_delete_event] = useState(false);
+function popup_delete_event() {
+        
+    const [show_delete_event, setShow_delete_event] = useState(false);
         const handleClose_delete_event = () => setShow_delete_event(false);
         const handleShow_delete_event = () => setShow_delete_event(true);
 
         return (
             <Modal
-                show={show_delete_event}
-                onHide={handleClose_delete_event}
-                backdrop="static"
-                keyboard={false}
-            >
-                <Modal.Header>
-                    <Modal.Title>Event löschen</Modal.Title>
-                </Modal.Header>
-                <Modal.Body>
-                    <h4> <BsExclamationTriangle />  &nbsp;  Ganz ganz wirklich ernsthaft sicher löschen? </h4>
-                </Modal.Body>
-                <Modal.Footer>
-                    <Button variant="success" onClick={handleClose_delete_event}>
-                        Doch nich!
-                    </Button>
-                    <Button variant="danger" onClick={handleClose_delete_event}>
-                        Jaaa!
-                    </Button>
-                </Modal.Footer>
-            </Modal>
-        )
-    }
+            show={show_delete_event}
+            onHide={handleClose_delete_event}
+            backdrop="static"
+            keyboard={false}
+        >
+            <Modal.Header>
+                <Modal.Title>Event löschen</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+                <h4> <BsExclamationTriangle />  &nbsp;  Ganz ganz wirklich ernsthaft sicher löschen? </h4>
+            </Modal.Body>
+            <Modal.Footer>
+                <Button variant="success" onClick={handleClose_delete_event}>
+                    Doch nich!
+                </Button>
+                <Button variant="danger" onClick={handleClose_delete_event}>
+                    Jaaa!
+                </Button>
+            </Modal.Footer>
+        </Modal>
+    )
 }
 
 export default {popup_new_event, popup_delete_event};
